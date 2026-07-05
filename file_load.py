0cdaf71598b49654088460c8aa1b11d2c8891799
Karl Gleason <solasfive@yahoo.com>
2026-07-05 16:18:46 -0500

    table creation and intial data load 4:18pm 7-5-2026

---
+++ file_load.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+intial_data = pd.read_csv('PowerBI_Portfolio_Financial_Dataset.csv')
+print(intial_data)
---
+++ sql/Region.sql
@@ -0,0 +1,14 @@
+use Executive_Dashboard
+
+-----------------------------------------------------------------------
+-- Intial Table creation for the Regions by Karl Gleason 7-5-2026
+-----------------------------------------------------------------------
+
+
+create table dbo.Region
+(Region_ID int
+,Region_name varchar(25)
+)
+
+
+
